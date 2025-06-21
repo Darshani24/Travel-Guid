ad89b0dc6e21dc426562d70db534e191d361d22c
Darshani <radkumari@std.appsc.sab.ac.lk>
2025-06-21 10:38:09 +0530

    Services section added

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Services = () => {
   return (
-  //  <section className="px-6 py-10 flex flex-col justify-center items-center">
-  //     <h1 className="text-3xl font-bold mb-6">Our Services</h1>
-  //     <ul className="space-y-4 text-lg">
-  //       <li>✅ Personalized Itinerary Creation</li>
-  //       <li>✅ Real-time Travel Updates</li>
-  //       <li>✅ Offline Travel Assistance</li>
-  //       <li>✅ Local Guide Recommendations</li>
-  //       <li>✅ Budget Tracking Tools</li>
-  //     </ul>
-  //   </section>
-  <div>Services Page</div>
+   <section className="px-6 py-10 flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-6">Our Services</h1>
+      <ul className="space-y-4 text-lg">
+        <li> Personalized Itinerary Creation</li>
+        <li> Real-time Travel Updates</li>
+        <li> Offline Travel Assistance</li>
+        <li> Local Guide Recommendations</li>
+        <li> Budget Tracking Tools</li>
+      </ul>
+    </section>
+  
   );
 }
 
