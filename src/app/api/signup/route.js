ba026434c23663ffaed61aee8519e054fbbf9c29
Darshani <radkumari@std.appsc.sab.ac.lk>
2025-06-22 07:38:02 +0530

    Connect Signup page with database

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,29 +1,29 @@
-// import { NextResponse } from "next/server";
-// import clientPromise from '../../../../lib/mongodb';
+import { NextResponse } from "next/server";
+import clientPromise from '../../../../lib/mongodb';
 
-// export async function POST(request) {
-//   try {
-//     const { name, email, password } = await request.json();
+export async function POST(request) {
+  try {
+    const { name, email, password } = await request.json();
 
-//     if (!name || !email || !password) {
-//       return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
-//     }
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+    }
 
-//     const client = await clientPromise;
-//     const db = client.db("User");
-
-   
-//     const existingUser = await db.collection("users").findOne({ email });
-//     if (existingUser) {
-//       return NextResponse.json({ error: "User already exists" }, { status: 409 });
-//     }
+    const client = await clientPromise;
+    const db = client.db("User");
 
    
-//     await db.collection("users").insertOne({ name, email, password });
+    const existingUser = await db.collection("users").findOne({ email });
+    if (existingUser) {
+      return NextResponse.json({ error: "User already exists" }, { status: 409 });
+    }
 
-//     return NextResponse.json({ message: "Signup successful!" });
-//   } catch (error) {
-//     console.error("Signup error:", error);
-//     return NextResponse.json({ error: "Server error" }, { status: 500 });
-//   }
-// }
+   
+    await db.collection("users").insertOne({ name, email, password });
+
+    return NextResponse.json({ message: "Signup successful!" });
+  } catch (error) {
+    console.error("Signup error:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
