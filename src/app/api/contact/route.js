697f03fecf7626505d5da694c65fc2ec7f9f64a9
Darshani <radkumari@std.appsc.sab.ac.lk>
2025-06-22 07:37:01 +0530

    Connect login page with database

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -3,7 +3,7 @@ import clientPromise from '../../../../lib/mongodb';
 export async function POST(req) {
   try {
     const client = await clientPromise;
-    const db = client.db(); // default db from URI
+    const db = client.db();
     const collection = db.collection('contacts');
 
     const { name, email, message } = await req.json();
--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,26 +1,26 @@
-// import { NextResponse } from "next/server";
-// import clientPromise from '../../../../lib/mongodb';
+import { NextResponse } from "next/server";
+import clientPromise from '../../../../lib/mongodb';
 
-// export async function POST(request) {
-//   try {
-//     const { email, password } = await request.json();
+export async function POST(request) {
+  try {
+    const { email, password } = await request.json();
 
-//     const client = await clientPromise;
-//     const db = client.db("User");
+    const client = await clientPromise;
+    const db = client.db("User");
 
-//     const user = await db.collection("users").findOne({ email });
-//     if (!user) {
-//       return NextResponse.json({ error: "User not found" }, { status: 401 });
-//     }
+    const user = await db.collection("users").findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 401 });
+    }
 
    
-//     if (user.password !== password) {
-//       return NextResponse.json({ error: "Invalid password" }, { status: 401 });
-//     }
+    if (user.password !== password) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 401 });
+    }
 
-//     return NextResponse.json({ message: "Login successful!" });
-//   } catch (error) {
-//     console.error("Login error:", error);
-//     return NextResponse.json({ error: "Server error" }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ message: "Login successful!" });
+  } catch (error) {
+    console.error("Login error:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
